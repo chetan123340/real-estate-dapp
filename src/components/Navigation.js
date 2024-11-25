f4dc57f74b30f28cf2a01f92671d745fd8c08463
CHETAN K.P <chetanrocks56@gmail.com>
2024-11-25 13:31:33 +0530

    minor changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,16 +8,16 @@ const Navigation = ({ account, setAccount }) => {
     }
 
     return (
-        <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Real Estate DApp</span>
-                <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Real Estate DApp</span>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     {account ? (
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-lg rounded-lg px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-lg rounded-lg px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             {account.slice(0, 6) + '...' + account.slice(38, 42)}
                         </button>
                     ) : (
-                            <button type="button" onClick={connectHandler} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connect
+                            <button type="button" onClick={connectHandler} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connect
                         </button>
                     )}
                 </div>
